ee65ee68709988c0059f3a1892f04f219dd3bcbc
Adam Cain <03adamcain@gmail.com>
2025-01-22 16:08:19 +0000

    feat: iframe message handling

--- packages/edge-v0/src/explorer/Explorer.tsx
+++ packages/edge-v0/src/explorer/Explorer.tsx
@@ -3,14 +3,19 @@ import { useTurboEdgeV0 } from "../hooks/useTurboEdgeV0";
 import TurboIcon from "./assets/TurboIcon";
 
 interface ExplorerProps {
-    explorer?: boolean | string;
+    iconSize?: number;
+    position?: 
+        | { top: string; left?: string; right?: never; bottom?: never }
+        | { bottom: string; left?: string; right?: never; top?: never }
+        | { left: string; top?: string; bottom?: never; right?: never }
+        | { right: string; top?: string; bottom?: never; left?: never };
 }
 
-export function Explorer({ explorer = false }: ExplorerProps) {
+export function Explorer({ iconSize = 18, position = { top: "1.25rem", left: "0" } }: ExplorerProps) {
     const turboEdge = useTurboEdgeV0();
     const [gameId, setGameId] = useState<string | null>(null);
     const [explorerUrl, setExplorerUrl] = useState<string>("");
-    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const [isOpen, setIsOpen] = useState<boolean>(true)
 
     useEffect(() => {
         if (turboEdge?.gameId) {
@@ -28,40 +33,40 @@ export function Explorer({ explorer = false }: ExplorerProps) {
 
     useEffect(() => {
         const handleIframeMessage = (event: MessageEvent) => {
-            if (event.data.type === 'iframe-error') {
-                console.error('Iframe Error:', event.data);
-                setIframeError(event.data.message);
-            }
+          if (event.data.type === 'iframe-message') {
+            setIframeError(event.data.message);
+          }
         };
-
+      
         window.addEventListener('message', handleIframeMessage);
-
+      
         return () => {
-            window.removeEventListener('message', handleIframeMessage);
+          window.removeEventListener('message', handleIframeMessage);
         };
-    }, []);
+      }, []);
 
     return (
         <div>
             <button
                 onClick={handleClick}
                 style={{
+                    ...position,
                     position: "absolute",
                     padding: '5px',
                     borderTopRightRadius: "0.5rem",
                     borderBottomRightRadius: "0.5rem",
-                    top: "1.25rem",
                     filter: "drop-shadow(0 10px 8px rgba(0, 0, 0, 0.04)) drop-shadow(0 4px 3px rgba(0, 0, 0, 0.1))",
                     background: "#FFFFFF",
                     zIndex: 9999
                 }}
             >
-                <TurboIcon size={18} color="#F13938" />
+                <TurboIcon size={iconSize} color="#F13938" />
             </button>
             <div
                 style={{
                     display: isOpen ? "block" : "none",
-                    position: "absolute", top: "0px", left: "0px",
+                    position: "absolute", 
+                    top: "0px", left: "0px",
                     zIndex: 9998,
                     background: "white",
                     width: "100%",
@@ -70,17 +75,25 @@ export function Explorer({ explorer = false }: ExplorerProps) {
             >
                 {
                     iframeError? 
-                        <div className="bg-red-100 text-red-800 p-4 rounded mb-4">
-                            <strong>Error in iframe:</strong> {iframeError}
-                        </div>                    
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            height: "100%",
+                            textAlign: "center",
+                            color: "#F13938",
+                            backgroundColor: "#FFEBEB",
+                        }}
+                    >
+                        <h1 style={{ fontSize: "2rem", marginBottom: "1rem" }}>Error</h1>
+                        <p style={{ fontSize: "1rem" }}>{iframeError}</p>
+                    </div>               
                     : <iframe
                     src={explorerUrl}
                     title="Turbo Explorer"
                     style={{ width: "100%", height: "100%", border: "none" }}
-                    onError={() => {
-                        console.error("Failed to load the iframe content.");
-                        alert("An error occurred while loading the content. Please try again later.");
-                    }}
                 ></iframe>
                 }
             </div>
--- packages/example-chat/src/main.tsx
+++ packages/example-chat/src/main.tsx
@@ -7,7 +7,7 @@ import { TurboEdgeProviderV0, Explorer } from '@turbo-ing/edge-v0'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TurboEdgeProviderV0 gameId='turbo-chat' daProxy='http://localhost:3000'>
-    <Explorer />
+      <Explorer />
       <App />
     </TurboEdgeProviderV0>
   </StrictMode>,
